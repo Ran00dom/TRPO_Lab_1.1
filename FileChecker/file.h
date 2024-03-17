8bec1fb777adef30920c8499ca7a7898b5b79982
Ran00dom <rran00domm@gmail.com>
2024-03-17 14:30:24 +0700

    Завершен FileInfoRecorder

--- FileChecker/file.h
+++ FileChecker/file.h
@@ -2,18 +2,25 @@
 #define FILE_H
 
 #include <QFileInfo>
-
+#include <QDateTime>
+#include <QObject>
 
 class FileInfoRecorder:public QFileInfo
 {
-
+    Q_OBJECT // макрос для компиляции слотов и сигналов
 private:
     bool exist = false;
-    QDateTime lastModifire();
+    QDateTime timeModified;
+    qint64 size;
+
     FileInfoRecorder* next;
 
 public:
-    FileInfoRecorder(const char* parth);
+    FileInfoRecorder(const char* parth, FileInfoRecorder* next);
+signals:
+    int logedStatus(QFileInfo* file);
+public slots:
+    bool updateData();
 
 };
 
