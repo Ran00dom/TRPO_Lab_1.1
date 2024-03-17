bb93e73bea20c00c50e1ccad35cc82f0871bff4e
Ran00dom <rran00domm@gmail.com>
2024-03-17 16:16:58 +0700

    В Loger определен logList

--- FileChecker/loger.cpp
+++ FileChecker/loger.cpp
@@ -4,6 +4,28 @@
 void Loger::logFileUpdate(QFileInfo* file)
 {
     if (file->exists())
-        qDebug() << file->baseName() << "STATUS / exist? " << file->exists() << " / size?  " << file->size() << ;
+        qDebug() << file->baseName() << "STATUS / exist? " << file->exists() << " / size?  " << file->size();
+}
 
+void Loger::logList(string name, int sizeX , int sizeY , string* columName, string** table)
+{
+    if (sizeX == 0 || sizeY == 0){
+        qDebug() << &name;
+    }
+    else{
+        qDebug() << "<< " << &name << ">>";
+    }
+
+    for (int i = 0; i < sizeX; i++)
+    {
+        cout << columName[i] << " | ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < sizeX; i++) {
+        for (int j = 0; j < sizeY; j++){
+            cout << table[i][j] << " | ";
+        }
+        cout << endl;
+    }
 }
